026ef33de6d9af8c5583908a9c925510ac8cec8a
Agster Junior Da Costa Santos <agster.santos01@gmail.com>
2024-05-28 20:21:42 -0300

    integração gpt

---
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -0,0 +1,24 @@
+package br.com.alura.screenmatch.service;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class ConsultaChatGPT {
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String OPENAI_API_KEY = dotenv.get("OPENAI_API_KEY");
+
+    public static String obterTraducao(String texto) {
+        OpenAiService service = new OpenAiService(OPENAI_API_KEY);
+
+        CompletionRequest requisicao = CompletionRequest.builder()
+                .model("gpt-3.5-turbo-instruct")
+                .prompt("traduza para o português o texto: " + texto)
+                .maxTokens(1000)
+                .temperature(0.7)
+                .build();
+
+        var resposta = service.createCompletion(requisicao);
+        return resposta.getChoices().get(0).getText();
+    }
+}
