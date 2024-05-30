a6662159ecd117acaf8d2d39f3dd73568652f16b
Agster Junior Da Costa Santos <agster.santos01@gmail.com>
2024-05-30 20:29:12 -0300

    persistencia de dados com jpa

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,12 +1,20 @@
 package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.principal.Principal;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
+
+
+	//anotação para o jpa gerenciar a interface por que não tem como instanciar e para a jpa gerenciar tem que ser em uma classe que o spring gerencie
+	@Autowired
+	private SerieRepository repositorio;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
@@ -16,7 +24,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		Principal principal = new Principal();
+		Principal principal = new Principal(repositorio);
 		principal.exibeMenu();
 
 
