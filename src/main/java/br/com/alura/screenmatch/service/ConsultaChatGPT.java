fd9b7aa6f2e1c4aa32414cc107373a875462ca05
Agster Junior Da Costa Santos <agster.santos01@gmail.com>
2024-05-29 22:17:33 -0300

    jpa

--- src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -2,14 +2,11 @@ package br.com.alura.screenmatch.service;
 
 import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
-import io.github.cdimascio.dotenv.Dotenv;
 
 public class ConsultaChatGPT {
-    private static final Dotenv dotenv = Dotenv.load();
-    private static final String OPENAI_API_KEY = dotenv.get("OPENAI_API_KEY");
-
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService(OPENAI_API_KEY);
+        OpenAiService service = new OpenAiService("");
+
 
         CompletionRequest requisicao = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
@@ -17,6 +14,7 @@ public class ConsultaChatGPT {
                 .maxTokens(1000)
                 .temperature(0.7)
                 .build();
+
 
         var resposta = service.createCompletion(requisicao);
         return resposta.getChoices().get(0).getText();
