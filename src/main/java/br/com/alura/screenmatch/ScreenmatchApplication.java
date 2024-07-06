ec2a6cdd79bfc0e1b732194cd5c685c032aa1dfc
Agster Junior Da Costa Santos <agster.santos01@gmail.com>
2024-07-06 18:42:12 -0300

    api resFull

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -8,32 +8,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
+public class ScreenmatchApplication  {
 
-
-	//anotação para o jpa gerenciar a interface por que não tem como instanciar e
-	// para a jpa gerenciar tem que ser em uma classe que o spring gerencie
-
-	//injeção de repositorio
-	@Autowired
-	private SerieRepository repositorio;
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
+
+
 	}
 
 
-	@Override
-	public void run(String... args) throws Exception {
 
-		Principal principal = new Principal(repositorio);
-		principal.exibeMenu();
 
 
 
 
 		}
 
-	}
+
 
---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplicationSemWeb.java
@@ -0,0 +1,39 @@
+//package br.com.alura.screenmatch;
+//
+//import br.com.alura.screenmatch.principal.Principal;
+//import br.com.alura.screenmatch.repository.SerieRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class ScreenmatchApplicationSemWeb implements CommandLineRunner {
+//
+//
+//	//anotação para o jpa gerenciar a interface por que não tem como instanciar e
+//	// para a jpa gerenciar tem que ser em uma classe que o spring gerencie
+//
+//	//injeção de repositorio
+//	@Autowired
+//	private SerieRepository repositorio;
+//
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ScreenmatchApplicationSemWeb.class, args);
+//	}
+//
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//
+//		Principal principal = new Principal(repositorio);
+//		principal.exibeMenu();
+//
+//
+//
+//
+//		}
+//
+//	}
+//
