4d578ff5550885b876a3ccf8976135a83db9f240
Agster Junior Da Costa Santos <agster.santos01@gmail.com>
2024-07-08 21:06:32 -0300

    dto

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,25 +1,36 @@
 package br.com.alura.screenmatch.controller;
 
 
-import br.com.alura.screenmatch.model.Serie;
-import br.com.alura.screenmatch.repository.SerieRepository;
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+
+//para não precisar ficar colocando /series
+@RequestMapping("/series")
 public class SerieController {
+
+
+    //injeção de dependencia
     @Autowired
-    private SerieRepository repository;
+    private SerieService servico;
 
-    @GetMapping("/series")
-    public List<Serie> obterSeries(){
-        return repository.findAll() ;
+    @GetMapping
+    public List<SerieDTO> obterSeries(){
+
+        //chamada do metodo da classe serieserviço graças a injeção de dependencia
+     return servico.obterTodasAsSeries();
     }
-
+   @GetMapping("/top5")
+    public List<SerieDTO> obterTop5Series(){
+        return servico.obterTop5Series();
+   }
 
 
 }
